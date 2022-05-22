3ebaf528fc624f1448340a58e846b4c5834421fc
zyimy <zepedayimy46@gmail.com>
2022-05-22 20:08:22 +0200

    primero

---
+++ src/test/java/com/desarrollo/securityapi/SecurityApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desarrollo.securityapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
